98f1356d97ae45aa4ac6fc04548f9be865919399
Ligture <2073377971@qq.com>
2023-08-03 19:23:12 +0800

    gui

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import encode
 import decode
-#retur = encode.encodefile(r"F:\xposed\nox-disk2.vmdk", '12345')
-retur = decode.decodefile(r"F:\xposed\REAbb5uE1sDVTj6gGc44LA==.enc", '12345')
+retur = encode.encodefile(r"F:\xposed\script.sh", '12345')
+#retur = decode.decodefile(r"F:\xposed\REAbb5uE1sDVTj6gGc44LA==.enc", '12345')
 print(retur)
---
+++ tkui.py
@@ -0,0 +1,6 @@
+import tkinter
+window = tkinter.Tk()
+window.title("GUI")
+window.geometry("640x400+100+100")
+window.resizable(False, False)
+window.mainloop()
